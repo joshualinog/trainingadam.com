53feecf48b6a4c1250e96a4ba7e9b90e5f0ede2d
Joshua Andrew Linog <jsinkcreations@gmail.com>
2026-01-07 22:31:10 +0000

    make various changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 export default {
   content: ["./src/**/*.{html,njk,js}"],
+  safelist: ['bg-seagrass-700'],
   theme: {
     extend: {
+      screens: {
+        'xs': '320px',
+      },
       fontFamily: {
         sans: ["Rubik", "ui-sans-serif", "system-ui", "sans-serif"],
+        hand: ["Gaegu", "cursive"],
       },
     },
   },
