1976ee92e4a409a1c78d3ea35a61bb8dd3001216
Joshua Andrew Linog <jsinkcreations@gmail.com>
2026-04-20 18:22:00 +0000

    blog page work. footer work.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 export default {
   content: ["./src/**/*.{html,njk,js}"],
-  safelist: ['bg-seagrass-700'],
+  safelist: [
+    { pattern: /bg-(golden-pollen|blazing-flame|burnt-peach|olive-bark|seagrass|night-bordeaux|cotton-candy|mauve-shadow|sunshine|glowyellowgreen)-\d{2,3}/ },
+  ],
   theme: {
     extend: {
       screens: {
